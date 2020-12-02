ac1f0d48d564267a4fd2fef8a06eaefb0b470be4
Kirill Morozov (moki) <morozov.kirill.moki@gmail.com>
2020-12-02 17:46:42 +0300

    fix(day_1): better solutions

--- day/day_1/main.cpp
+++ day/day_1/main.cpp
@@ -1,37 +1,93 @@
+#include <algorithm>
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <vector>
 
-int main() {
-        std::fstream input("input");
+using namespace std::chrono;
+
+uint64_t sliding_pointer_part1(const std::vector<uint64_t> &ns,
+                               uint64_t target) {
+        for (auto i = ns.begin(); i != ns.end(); i++) {
+                for (auto j = i + 1, k = ns.end() - 1; j < k;) {
+                        auto mid = (j + (k - j) / 2);
+                        auto sum = *i + *mid;
+
+                        if (sum == target) {
+                                std::cout << "pair: (" << *i << ", " << *mid
+                                          << ")" << std::endl;
+
+                                return *i * *mid;
+                        }
+
+                        if (sum < target) {
+                                j = mid + 1;
+                        } else {
+                                k = mid - 1;
+                        }
+                }
+        }
+
+        return 0;
+}
+
+uint64_t sliding_pointer_part2(std::vector<uint64_t> &ns, uint64_t target) {
+        for (auto i = ns.begin(); i != ns.end(); i++) {
+                for (auto j = i + 1, k = ns.end() - 1; j < k;) {
+                        auto sum = *i + *j + *k;
+                        if (sum == target) {
+                                std::cout << "triplet: (" << *i << ", " << *j
+                                          << ", " << *k << ")" << std::endl;
+                                return *i * *j * *k;
+                        }
+
+                        if (sum < target) {
+                                j++;
+                        } else {
+                                k--;
+                        }
+                }
+        }
+
+        return 0;
+}
+
+constexpr uint64_t t = 2020;
+
+int main(int argc, char **argv) {
+        std::fstream input(*++argv);
         if (!input) {
                 exit(1);
         }
 
-        uint32_t n;
-        std::vector<uint32_t> ns{};
+        uint64_t n;
+        std::vector<uint64_t> ns{};
 
         for (; input >> n;) {
                 ns.push_back(n);
         }
 
-        /* part 1 */
-        for (auto i = ns.begin(); i != ns.end(); i++) {
-                for (auto j = i + 1; j != ns.end(); j++) {
-                        if (*i + *j == 2020) {
-                                std::cout << *i * *j << std::endl;
-                        }
-                }
-        }
+        std::sort(ns.begin(), ns.end());
 
-        /* part 2 lazy to hashmap sums, godbless moores law */
-        for (auto i = ns.begin(); i != ns.end(); i++) {
-                for (auto j = i + 1; j != ns.end(); j++) {
-                        for (auto k = j + 1; k != ns.end(); k++) {
-                                if (*i + *j + *k == 2020) {
-                                        std::cout << *i * *j * *k << std::endl;
-                                }
-                        }
-                }
-        }
+        // part 1
+        auto before = std::chrono::high_resolution_clock::now();
+        auto solution = sliding_pointer_part1(ns, t);
+        auto after = std::chrono::high_resolution_clock::now();
+
+        auto took = duration_cast<microseconds>(after - before);
+        std::cout << "bsearch part 1 solution: " << took.count() << "μs"
+                  << std::endl;
+
+        std::cout << "solution: " << solution << std::endl;
+
+        // part 2
+        before = std::chrono::high_resolution_clock::now();
+        solution = sliding_pointer_part2(ns, t);
+        after = std::chrono::high_resolution_clock::now();
+
+        took = duration_cast<microseconds>(after - before);
+        std::cout << "sliding window part 2 solution: " << took.count() << "μs"
+                  << std::endl;
+
+        std::cout << "solution: " << solution << std::endl;
 }
